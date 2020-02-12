4360169aaaf83e8fd29f5fa3a459932b24409583
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-12 15:30:19 +0900

    本番環境のみで認証するよう修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 server '18.177.240.72', user: 'ec2-user', roles: %w{app db web}
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}

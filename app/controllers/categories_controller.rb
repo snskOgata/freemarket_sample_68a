b2696ed89225bb84800032f67f5580be5e7b8608
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-13 14:17:04 +0900

    カテゴリラベルの追加 親要素を持つ場合それも一緒に表示する

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,10 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @children = @category.children
-    @parent = @children.parent
+    @parent = @category.parent
+    # さらに親要素を持つならそれを代入
+    if @parent
+      @parent = @parent.parent.present? ? @parent.parent : @parent
+    end
   end
 end

89aa1aaa6731de89b27121d42e9dd14c6efd8202
eiichi-mamada <eastrage@gmail.com>
2020-02-13 20:19:59 +0900

    ユーザーモデルのリレーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-
+  has_many :sales
+  has_many :orders
+  has_many :credits,     dependent: :destroy
+  has_one :profile,      dependent: :destroy
+  has_one :user_address, dependent: :destroy
 end

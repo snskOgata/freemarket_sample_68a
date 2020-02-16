1984a20ba23b1f26d05de89a1a3d39dd5aa94a88
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-16 21:15:05 +0900

    出品ページのマークアップを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#show'
 
-  resources :category, controller: :categories, only: [:index, :show]
+  resources :category, controller: :categories, only: [:index, :show, :new]
 end

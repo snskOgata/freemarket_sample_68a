a6252d08e24589c134e50575a7f365627e4bf08b
eiichi-mamada <eastrage@gmail.com>
2020-02-16 12:27:15 +0900

    バリデーションの追記

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,19 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
+
+  # 全角ひらがなカタカナ
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/ 
+  # 郵便番号（ハイフンあり7桁）
+  VALID_POSTAL_REGEX = /\A\d{3}[-]\d{4}\z/
+  # 全て数値（全角）
+  VALID_NUMBER_REGEX = /\A[０-９]+\z/
+  # 携帯番号(ハイフンなし10桁or11桁)
+  VALID_PHONE_REGEX = /\A\d{10,11}\z/
+
+  validates :postal_code,  presence: true,   format:{ with: VALID_POSTAL_REGEX }
+  
+  validates :city,         presence: true,   format:{ with: VALID_NAME_REGEX }
+  validates :block_number, presence: true,   format:{ with: VALID_NUMBER_REGEX }
+  validates :phone_number,                   format:{ with: VALID_PHONE_REGEX }, allow_blank: true
+  validates :building,                       format:{ with: VALID_NAME_REGEX },  allow_blank: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,15 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
+
+  # 全角ひらがなカタカナ
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/ 
+  # 全角カタカナ
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+
+  validates :lastname,       presence: true, format: { with: VALID_NAME_REGEX }
+  validates :firstname,      presence: true, format: { with: VALID_NAME_REGEX }
+  validates :lastname_kana,  presence: true, format: { with: VALID_KANA_REGEX }
+  validates :firstname_kana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :birthday,       presence: true
+
 end

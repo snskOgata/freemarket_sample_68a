d8e43f7784990ffa21ca514a0cbe159123392640
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-17 16:38:37 +0900

    categoryとsaleのアソシエーションの追記

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,4 +2,7 @@ class Sale < ApplicationRecord
   validates :name, :detail, :condition_id, :delivery_payer_id, 
     :prefecture_id, :prep_days_id, :price, :status_id, presence: true
   validates :name, length: { maximum: 40 }
+
+  has_many: :category_sales
+  has_many: :categories, through: :category_sales
 end

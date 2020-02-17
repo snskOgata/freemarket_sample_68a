4391036fc2198946a463be2529bc7411019b75ec
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-17 16:10:37 +0900

    Saleのマイグレーションファイルの完成

--- db/migrate/20200217065253_create_sales.rb
+++ db/migrate/20200217065253_create_sales.rb
@@ -1,7 +1,14 @@
 class CreateSales < ActiveRecord::Migration[5.2]
   def change
     create_table :sales do |t|
-
+      t.string :name, null: false, limit: 40
+      t.text :detail, null: false
+      t.integer :condition_id, null: false
+      t.integer :delivery_payer_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :prep_days_id, null: false
+      t.integer :price, null: false
+      t.integer :status_id, null: false, default: 0
       t.timestamps
     end
   end

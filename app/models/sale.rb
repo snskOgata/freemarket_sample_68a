73a48fc705276f3eba1517e66e37b295c670defc
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-17 16:31:06 +0900

    Saleモデルのバリデーションを追加

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,5 @@
 class Sale < ApplicationRecord
+  validates :name, :detail, :condition_id, :delivery_payer_id, 
+    :prefecture_id, :prep_days_id, :price, :status_id, presence: true
+  validates :name, length: { maximum: 40 }
 end

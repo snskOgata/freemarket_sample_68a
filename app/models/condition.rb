2981831b59ee7fe13323fa4b7090b585272be94c
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-18 18:36:50 +0900

    出品に関するActiveHashの紐つけを完成

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,7 +1,7 @@
 class Condition < ActiveHash::Base
   self.data = [
-      {id: 1, name: '新品、未使用'}, {id: 2, name: '未使用に近い'},
-      {id: 3, name: '目立った傷や汚れなし'}, {id: 4, name: 'やや傷や汚れあり'},
-      {id: 5, name: '傷や汚れあり'}, {id: 6, name: '全体的に状態が悪い'}
+      {id: 1, type: '新品、未使用'}, {id: 2, type: '未使用に近い'},
+      {id: 3, type: '目立った傷や汚れなし'}, {id: 4, type: 'やや傷や汚れあり'},
+      {id: 5, type: '傷や汚れあり'}, {id: 6, type: '全体的に状態が悪い'}
   ]
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,9 +4,12 @@ class Sale < ApplicationRecord
     :prefecture_id, :prep_days_id, :price, :status_id, presence: true
   validates :name, length: { maximum: 40 }
 
-  has_many: :category_sales
-  has_many: :categories, through: :category_sales
-  has_many: :photos
+  has_many :category_sales
+  has_many :categories, through: :category_sales
+  has_many :photos
 
   belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_payer
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :prep_days
 end

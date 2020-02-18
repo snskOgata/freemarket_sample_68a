7e41d4a937168735d237e057333cdaf0e038d498
snskOgata <58130661+snskOgata@users.noreply.github.com>
2020-02-18 15:18:06 +0900

    Feature/sales function/sales#new markup (#13)
    * 出品ページのマークアップを作成
    
    * 背景色の変更
    
    * マージ時に生じた問題の解消

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,5 +1,9 @@
 class SalesController < ApplicationController
+  
   def index
-    
   end
+
+  def new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'sales#index'
-  root to: 'homes#show'
   resources :category, controller: :categories, only: [:index, :show]
+  resources :sales, only: :new
 end

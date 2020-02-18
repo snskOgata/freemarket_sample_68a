d7ef40bdebe73b9abb9ede2d86e5107f50fe0879
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-18 17:27:56 +0900

    Saleモデルにcondition要素をActiveHashで付与

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,5 @@
 class Sale < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   validates :name, :detail, :condition_id, :delivery_payer_id, 
     :prefecture_id, :prep_days_id, :price, :status_id, presence: true
   validates :name, length: { maximum: 40 }
@@ -6,4 +7,6 @@ class Sale < ApplicationRecord
   has_many: :category_sales
   has_many: :categories, through: :category_sales
   has_many: :photos
+
+  belongs_to_active_hash :condition
 end

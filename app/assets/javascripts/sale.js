b628f287486d131341523fad0aa3f91acc279240
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-19 10:58:21 +0900

    大カテゴリを選択すると中カテゴリを表示(#bug有)

--- app/assets/javascripts/sale.js
+++ app/assets/javascripts/sale.js
@@ -1,8 +1,13 @@
 $(function () {
+
+  // カテゴリ一覧を保持する変数
   var categories = []
-  var first_num = -1
-  var second_num = -1
-  var third_num = -1
+  // 大中小カテゴリごとの順番を保持
+  var first_num = 0
+  var second_num = 0
+  var third_num = 0
+
+  // ページ遷移後にカテゴリ一覧を取得
   $.getJSON(
     '/category',
     function (data) {
@@ -14,8 +19,34 @@ $(function () {
         op.value = categories[i].id;  //value値
         op.text = categories[i].name;   //テキスト値
         op.dataset.num = i
-        document.getElementById("category-first").appendChild(op);
+        $("#category-first").append(op);
       }
     }
   );
+
+  // 大カテゴリに変更があれば発火
+  $('#category-first').change(function () {
+
+    var second = $('#category-second')
+    var third = $('#category-third')
+    second.children().remove();
+    third.children().remove();
+    // 選択した番号を
+    first_num = $("#category-first option:selected").data("num");
+    for (var i = 0; i < categories.length; i++) {
+      var op = document.createElement("option");
+      op.value = categories[first_num].sub[i].id;
+      op.text = categories[first_num].sub[i].name;
+      op.dataset.num = [i];
+      second.append(op)
+    }
+    if (first_num !== 0) {
+      second.show();
+      third.hide();
+    } else {
+      second.hide();
+      third.hide();
+    }
+  });
+
 })

3b69ce23a0641100418a62f33a6163938b74145c
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-19 12:59:21 +0900

    カテゴリへの入力がない場合アラートを表示

--- app/assets/javascripts/sale.js
+++ app/assets/javascripts/sale.js
@@ -26,19 +26,21 @@ $(function () {
 
   // 大カテゴリに変更があれば発火
   $('#category-first').change(function () {
-
-    var second = $('#category-second')
-    var third = $('#category-third')
+    // 子要素・孫要素を削除
+    second_num = -1;
+    third_num = -1;
+    var second = $('#category-second');
+    var third = $('#category-third');
     second.children().remove();
     third.children().remove();
-    // 選択した番号を
+    // 選択した番号を保持
     first_num = $("#category-first option:selected").data("num");
     for (var i = 0; i < categories[first_num].sub.length; i++) {
       var op = document.createElement("option");
       op.value = categories[first_num].sub[i].id;
       op.text = categories[first_num].sub[i].name;
       op.dataset.num = [i];
-      second.append(op)
+      second.append(op);
     }
     if (first_num !== 0) {
       second.show();
@@ -51,23 +53,37 @@ $(function () {
 
   // 中カテゴリに変更があれば発火
   $('#category-second').change(function () {
-
+    // 子要素を削除
     var third = $('#category-third')
     third.children().remove();
-    // 選択した番号を
-    second_num = $("#category-first option:selected").data("num");
+    // 選択した番号を保持
+    second_num = $("#category-second option:selected").data("num");
     for (var i = 0; i < categories[first_num].sub[second_num].sub.length; i++) {
       var op = document.createElement("option");
       op.value = categories[first_num].sub[second_num].sub[i].id;
       op.text = categories[first_num].sub[second_num].sub[i].name;
-      third.append(op)
+      third.append(op);
     }
     if (first_num !== 0) {
       third.show();
     } else {
       third.hide();
     }
+    third_num = -1;
   });
 
+  // 小カテゴリに変更があれば発火
+  $('#category-third').change(function () {
+    // 選択した番号を保持
+    third_num = $("#category-third option:selected").data("num");
+  })
+
+  // 出品ボタンを押した時に、カテゴリを選択していないと忠告する
+  $('#submit-sale').on("click", function (e) {
+    if ((first_num < 1) || (second_num < 1) || (third_num < 1)) {
+      e.preventDefault();
+      alert('カテゴリは全て入力してください')
+    }
+  })
 
 })

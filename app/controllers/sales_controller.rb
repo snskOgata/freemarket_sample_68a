6f41776cea501ff77a0ecb227dfdfa4801561097
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-19 12:28:30 +0900

    画像無しでの出品機能を完了

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -8,6 +8,16 @@ class SalesController < ApplicationController
   end
 
   def create
+    @sale = Sale.new(sale_params)
+    if @sale.save
+      redirect_to root_path, notice: '商品を出品しました'
+    else
+      render :new
+    end
   end
-  
+
+  private 
+    def sale_params
+      params.require(:sale).permit(:name, :detail, :condition_id, :delivery_payer_id, :prefecture_id, :prep_days_id, :price, category_ids: []).merge(seller_id: current_user.id)
+    end
 end

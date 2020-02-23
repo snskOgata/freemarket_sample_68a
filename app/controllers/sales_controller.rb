113214aeae98e59c9e14173d49d41e9518c9e9a3
Yni-05 <yhforwork1@gamil.com>
2020-02-24 04:49:54 +0900

    salesのhamlとcontrollerの変更

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,8 +2,7 @@ class SalesController < ApplicationController
   
   def index
     @main_categories = Category.where(id: 1..13)
-
-    # @children = @main_category.@children.where(id: 1..14)
+    @sales = Sales.order(created_at: :desc).limit(3)
   end
 
   def new

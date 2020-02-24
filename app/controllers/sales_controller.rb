2d473b6dc739e4f25003447b7f8e5875cb66a136
Yni-05 <yhforwork1@gamil.com>
2020-02-24 14:02:28 +0900

    haml商品一覧表示

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,7 +2,7 @@ class SalesController < ApplicationController
 
   def index
     @main_categories = Category.where(id: 1..13)
-    @sales = Sales.order(created_at: :desc).limit(3)
+    @sales = Sale.order(created_at: :desc).limit(3)
   end
 
   def new

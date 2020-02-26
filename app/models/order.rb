17be668d311f4d5194a81cddf2329956c2023249
eiichi-mamada <eastrage@gmail.com>
2020-02-26 13:07:21 +0900

    orderテーブルへの保存

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
-  belongs_to :buyer
+  belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id', optional: true
+  belongs_to :sale,                      foreign_key: true,       optional: true
 end

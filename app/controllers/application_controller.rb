eaef50ec385305636a6a54c5cd1750ac04dd6908
Yni-05 <yhforwork1@gamil.com>
2020-02-27 21:56:27 +0900

    omniauthに関する記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   protected
     
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname, :lastname, :birthday])
   end
 
 end

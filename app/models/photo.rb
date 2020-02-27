0036a7c203243ed96d1e1340e27396c12a3da238
snskOgata <58130661+snskOgata@users.noreply.github.com>
2020-02-27 14:02:47 +0900

    不要なファイルを削除 (#31)
    * photoのデータ削除時に、画像ファイルを削除
    
    * テスト時の画像保存先を変更
    
    * テスト終了時に、テストで作成された画像ファイル群を削除

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,11 @@
 class Photo < ApplicationRecord
   belongs_to :sale, optional: true
   mount_uploader :image, ImageUploader
+  before_destroy :remove_image
+
+  def remove_image
+    image.remove!
+  rescue Exception => e
+    logger.error(e.message)
+  end
 end

e5e2a3feb33a1dcc7406b7411111ca29ae7e0983
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-28 13:56:09 +0900

    コントローラで必要なデータを用意

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,25 @@
 class ListingsController < ApplicationController
+  before_action :set_categories
   def listing
+    @status = 0
+    @sales = current_user.sales.where(status: @status)
+    render 'listing'
   end
 
   def in_progress
+    @status = 1
+    @sales = current_user.sales.where(status: @status)
+    render 'listing'
   end
 
   def completed
+    @status = 2
+    @sales = current_user.sales.where(status: @status)
+    render 'listing'
   end
+
+  private
+    def set_categories
+      @main_categories = Category.where(id: 1..13)
+    end
 end

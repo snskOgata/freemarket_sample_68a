610de35352903ef5288c86d6638e065a40ebafc4
eiichi-mamada <eastrage@gmail.com>
2020-02-28 11:50:21 +0900

    cardモデルのバリデーションの付与、単体テスト

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :customer_id, presence: true
+  validates :card_id    , presence: true
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
+describe Card do
+  describe '#create' do
+    it "customer_idとcard_idがあれば保存できる", focus: true do
+      card = build(:card)
+      expect(card).to be_valid
+    end
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "customer_idが空だと保存できない", focus: true do
+      card = build(:card, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+
+    it "card_idが空だと保存できない", focus: true do
+      card = build(:card, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
+
+  end
 end

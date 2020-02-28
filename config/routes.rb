371804b45bac6c3e464b4026c01cf446ac053923
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-28 13:55:30 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'listings/listing'
-  get 'listings/in_progress'
-  get 'listings/completed'
   # ユーザ関連
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
@@ -24,6 +21,11 @@ Rails.application.routes.draw do
   resource :mypage, only: [:show]
   scope :mypage do
     resources :cards, only: [:new, :index, :destroy, :create]
+    namespace :listings do
+      get 'listing'
+      get 'in_progress'
+      get 'completed'
+    end
   end
 
   get '*anything', to: 'errors#error_page'

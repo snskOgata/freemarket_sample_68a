5c41a2f9f2103bd8b1aeb5831fe45d632799f363
Yni-05 <yhforwork1@gamil.com>
2020-02-28 15:01:54 +0900

    ログインボタンを追加

--- app/assets/javascripts/channels/api_facebool.js
+++ app/assets/javascripts/channels/api_facebool.js
@@ -33,3 +33,14 @@ FB.getLoginStatus(function(response) {
       userID:'...'
   }
 }
+
+<fb:login-button 
+  scope="public_profile,email"
+  onlogin="checkLoginState();">
+</fb:login-button>
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}

8376342a0530e4d078af6ae682375a3c515bd41e
eiichi-mamada <eastrage@gmail.com>
2020-03-03 12:38:41 +0900

    cardモデルの単体テスト

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,21 +1,27 @@
 require 'rails_helper'
 describe Card do
   describe '#create' do
-    it "customer_idとcard_idがあれば保存できる", focus: true do
+    it "customer_idとcard_idがあれば保存できる", focus: false do
       card = build(:card)
       expect(card).to be_valid
     end
 
-    it "customer_idが空だと保存できない", focus: true do
+    it "customer_idが空だと保存できない", focus: false do
       card = build(:card, customer_id: "")
       card.valid?
       expect(card.errors[:customer_id]).to include("を入力してください")
     end
 
-    it "card_idが空だと保存できない", focus: true do
+    it "card_idが空だと保存できない", focus: false do
       card = build(:card, card_id: "")
       card.valid?
       expect(card.errors[:card_id]).to include("を入力してください")
+    end
+
+    it "user_idが空だと保存できない", focus: false do
+      card = build(:card, user_id: "")
+      card.valid?
+      expect(card.errors[:user_id]).to include("を入力してください")
     end
 
   end

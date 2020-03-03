5d00f17816cfa0557e777dd1bc2c8c3f1115b989
josh-aki <59858700+josh-aki@users.noreply.github.com>
2020-03-03 20:25:23 +0900

    商品購入履歴の表示 (#44)
    * 購入履歴用のrouting, action, viewを設定
    
    取引中と過去の取引の二つがある
    
    * 遷移先のリンクを指定
    
    * 購入履歴を、取引中と過去の取引に表示

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -18,6 +18,27 @@ class ListingsController < ApplicationController
     render 'listing'
   end
 
+  def purchasing
+    @status = 1
+    orders = current_user.orders
+    @sales =[]
+    orders.each do |order|
+      sale = Sale.where(id: order.sale_id, status: @status)[0]
+      @sales << sale if sale.present?
+    end
+  end
+
+  def purchased
+    @status = 2
+    orders = current_user.orders
+    @sales =[]
+    orders.each do |order|
+      sale = Sale.where(id: order.sale_id, status: @status)[0]
+      @sales << sale if sale.present?
+    end
+    render 'purchasing'
+  end
+
   private
     def set_categories
       @main_categories = Category.where(id: 1..13)

a4be934338244df6004c3b85d1f0303a1dd9548b
eiichi-mamada <eastrage@gmail.com>
2020-03-03 20:48:54 +0900

    不要な記述削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,4 @@
 FactoryBot.define do
-
-  # factory :user do
-  #   nickname              {"eiichi"}
-  #   email                 {"eee@gmail.com"}
-  #   password              {"00000000"}
-  #   password_confirmation {"00000000"}
-  # end
-
   factory :user do
     password = Faker::Internet.password(8)
     nickname { Faker::Name.last_name }

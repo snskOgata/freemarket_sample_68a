cedfcb8c3caf7d9e3cfc3091226b5b54b1e8cd40
eiichi-mamada <59813582+eiichi-mamada@users.noreply.github.com>
2020-03-04 14:05:56 +0900

    orderモデルおよびcardモデルの単体テスト (#46)
    * 購入画面のマークアップ
    
    色彩等微調整必要
    
    * cardモデルの生成
    
    * payjp bundle install
    
    * cards controllerの生成
    
    * gem 'dotenv-rails'　インストール
    
    * クレジットカード登録、削除の実装
    
    * クレデンシャルの更新
    
    * クレデンシャルを変数に適応
    
    * ビューファイルのusers/showをmypages/indexに移動させmain_headerの追記
    
    * 支払い方法からクレジットカード登録のリンク作成
    
    * 購入ページへルーティング
    
    * user,profile,addressモデルのテストを日本語化修正
    
    * cardモデルのバリデーションの付与、単体テスト
    
    * cardモデルの単体テスト
    
    * クレデンシャル再登録、fakerインストール
    
    * 単体テスト修正　不要なファイル削除
    
    * card,orderモデルのバリデーション修正
    
    * 不要な記述削除
    
    * Delete index.html.haml
    
    * Delete cards.coffee

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
-  belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id', optional: true
+  belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id'
   belongs_to :sale,                      foreign_key: true,       optional: true
+
+  validates :buyer_id, presence: true
+  validates :sale_id , presence: true, uniqueness: true
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
+describe Order do
+  describe '#create' do
+    it "buyer_idとsale_idがあれば保存できる" do
+      user = create(:user, id: 1)
+      sale = create(:sale, id: 1)
+      order = build(:order)
+      expect(order).to be_valid
+    end
 
-RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "buyer_idが空だと保存できない" do
+      user = create(:user, id: 1)
+      sale = create(:sale, id: 1)
+      order = build(:order, buyer_id: "")
+      order.valid?
+      expect(order.errors[:buyer_id]).to include("を入力してください")
+    end
+
+    it "sale_idが空だと保存できない" do
+      user = create(:user, id: 1)
+      sale = create(:sale, id: 1)
+      order = build(:order, sale_id: "")
+      order.valid?
+      expect(order.errors[:sale_id]).to include("を入力してください")
+    end
+
+    it "sale_idが重複すると保存できない(バリデーションエラーが起こる)" do
+      expect do
+      user = create(:user, id: 1)
+      sale = create(:sale, id: 1) 
+      order1 = create(:order, sale_id: 1)
+      order2 = create(:order, sale_id: 1)
+      end.to raise_error( ActiveRecord::RecordInvalid )
+    end
+
+    it "buyer_idが重複しても保存できる" do
+      sale1 = create(:sale, id: 1) 
+      sale2 = create(:sale, id: 2)
+      user = create(:user, id: 1)
+      order1 = create(:order, buyer_id: 1, sale_id: 1)
+      order2 = build(:order, buyer_id: 1, sale_id: 2)
+      expect(order2).to be_valid
+    end
+
+  end
 end
